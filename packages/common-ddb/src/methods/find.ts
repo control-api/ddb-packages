fe49f24a2593f530fd296387ca209f170906d996
Maksym-Haiovets <makcumyt@gmail.com>
2022-06-13 20:59:51 +0300

    fix: Fixed type-check

--- packages/common-ddb/src/methods/find.ts
+++ packages/common-ddb/src/methods/find.ts
@@ -1,12 +1,11 @@
 import type {DocumentClient} from 'aws-sdk/lib/dynamodb/document_client';
-import DynamoDB from 'aws-sdk/clients/dynamodb';
 import {query} from '../ddb';
 
 export async function find<T>({
   params,
 }: {
   params: DocumentClient.QueryInput;
-  key: DynamoDB.DocumentClient.Key | undefined;
+  key: DocumentClient.Key | undefined;
 }): Promise<T[]> {
   const {Items} = await query(params);
 
