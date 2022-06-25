515f2dfc9c735a451ec105c5195f907bbd49cc3e
Maksym-Haiovets <makcumyt@gmail.com>
2022-06-25 16:18:16 +0300

    feat: Refactored payment transactions facet

--- packages/payment-transactions/src/payment-transactions/write/insert-transactions.ts
+++ packages/payment-transactions/src/payment-transactions/write/insert-transactions.ts
@@ -3,12 +3,12 @@ import {insertMany} from '@control-api/common-ddb';
 import {PaymentTransactions} from '@control-api/types-ddb';
 import {getTransactionFacetKey, getTransactionFacetType, tableName} from '../../table-helpers';
 
-export async function insertTransactions(userId: string, transactions: PaymentTransactions.Transaction[]): Promise<void> {
+export async function insertTransactions(userId: string, cardNumber: string, transactions: PaymentTransactions.Transaction[]): Promise<void> {
   const ddbTransactions: PaymentTransactions.DDBTransaction[] = transactions.map((transaction) => {
     const extId = transaction.id;
     const id = ulid();
     const ddbTransaction: PaymentTransactions.DDBTransaction = {
-      ...getTransactionFacetKey(userId, id),
+      ...getTransactionFacetKey(userId, cardNumber, id),
       facetType: getTransactionFacetType(),
       ...transaction,
       extId,
